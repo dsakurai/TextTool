811a2e52720c85a887d44f0f82b11114473a3e8a
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-11-01 05:40:05 +0900

    Initial commit

---
+++ TextTool/Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+open System.Net
+
+Console.Write("Enter text: ")
+
+let originalString           =  Console.ReadLine() // Get user input, like "text@domain"
+let webEncodedString: string = WebUtility.UrlEncode(originalString)
+
+printfn "%s" webEncodedString
